a332ea466803df0bcf682f4527bbd547d09945fb
Nadarajah Mag-Stellon <magstellon@gmail.com>
2019-02-08 17:50:35 +0100

    Draft #1 - WebSocket

--- src/main/java/com/magstellon/openchatroom/message/MessageController.java
+++ src/main/java/com/magstellon/openchatroom/message/MessageController.java
@@ -3,6 +3,7 @@ package com.magstellon.openchatroom.message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class MessageController {
